7cdcf827a83e683f56043b4b5206bca0335f78a7
Diego <diego.yactayo@hotmail.com>
2020-08-02 23:26:48 -0500

    prueba subida

--- src/App.js
+++ src/App.js
@@ -1,24 +1,67 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import clsx from 'clsx';
+import { makeStyles } from "@material-ui/core/styles";
+import Card from "@material-ui/core/Card";
+import List from "@material-ui/core/List";
+import ListItem from "@material-ui/core/ListItem";
+import ListItemText from "@material-ui/core/ListItemText";
+import Divider from "@material-ui/core/Divider";
+import Typography from "@material-ui/core/Typography";
+import { green } from '@material-ui/core/colors';
+import AddIcon from '@material-ui/icons/Add';
+import TextField from '@material-ui/core/TextField';
+import InputAdornment from '@material-ui/core/InputAdornment';
+const useStyles = makeStyles((theme) => ({
+  root: {
+    width: "100%",
+    maxWidth: 360,
+    backgroundColor: theme.palette.background.paper,
+  },
+  '& > span': {
+    margin: theme.spacing(2),
+  },
+}));
+
 
 function App() {
+  const classes = useStyles();
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+      <Card className={classes.root}>
+        <List
+          component="nav"
+          className={classes.root}
+          aria-label="mailbox folders"
         >
-          Learn React
-        </a>
-      </header>
+          <ListItem>
+            <ListItemText primary="" />
+            <Typography variant="h5" gutterBottom>
+              TALLER
+            </Typography>
+          </ListItem>
+          <Divider />
+          <ListItem button>
+          <TextField
+          id="standard-textarea"
+          label="Registro de servicio"
+          placeholder="Placeholder"
+          multiline
+        />
+         <TextField
+          label="Monto"
+          id="standard-start-adornment"
+          className={clsx(classes.margin, classes.textField)}
+          InputProps={{
+            startAdornment: <InputAdornment position="start">S/</InputAdornment>,
+          }}
+        />
+          <AddIcon></AddIcon> Añadir otro registro
+          </ListItem>
+        </List>
+      </Card>
+
     </div>
   );
 }
